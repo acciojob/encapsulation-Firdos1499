c64e14df59f332a117574d753b10232c240164fb
Firdos Ansari <96837366+Firdos1499@users.noreply.github.com>
2022-10-07 15:04:16 +0530

    Update RWOnly.java

--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -2,4 +2,12 @@ package com.driver;
 
 public class RWOnly {
     Private String name;
+    
+     void setName(String name){
+       this.name = name;
+    }
+    
+     String getName(){
+     return name;
+    }
 }
