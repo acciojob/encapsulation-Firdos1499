fd71ff30526ae86204360d5a2bd45f0b70476641
Firdos Ansari <96837366+Firdos1499@users.noreply.github.com>
2022-10-07 15:07:38 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,6 +3,7 @@ package com.driver;
 public class Main {
   public static void main(String args[]){
      RWOnly rw1 = new RWOnly();
-     
+     System.out.println(rw1.name);
+    // name has private access in RWOnly
   }
 }
